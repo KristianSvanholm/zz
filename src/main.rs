5364c425a9216622c9e6a5322f886ec94d87742b
kristian <kristianrorensvanholm@gmail.com>
2025-04-17 13:58:13 +0200

    calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use chrono::{Duration, Timelike};
+
 fn main() {
-    println!("Hello, world!");
+   
+    let cycle_count = 6;
+    let now = chrono::offset::Local::now();
+    let nod_time = Duration::minutes(15);
+    let cycle_time = Duration::minutes(90);
+
+    for i in 1..cycle_count+1 {
+        let cycles = cycle_time.checked_mul(i).expect("ooh");
+        let itt = now + nod_time + cycles;
+        println!("{:0>2}:{:0>2}:{:0>2}", itt.hour(), itt.minute(), itt.hour());
+    }
 }
